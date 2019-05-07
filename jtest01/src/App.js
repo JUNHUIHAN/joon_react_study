e4345bffd88247a482a1eedceeeb45ec3333f184
JUNHUIHAN <49679828+JUNHUIHAN@users.noreply.github.com>
2019-05-07 20:43:20 +0900

    실습환경 구축&html실습
    간단한 html 실습 해보리기

---
+++ jtest01/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Joon Hee 존잘레스 <br></br>
+        린정 어린정
+      </div>
+    );
+  }
+}
+
+export default App;
